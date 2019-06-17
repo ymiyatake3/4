9e4e207db94abf50c3c0b0b704c421f9bee89ddc
yoko <miyatakeyouko@gmail.com>
2019-06-17 20:25:41 +0900

    make getLinks function

--- sns.go
+++ sns.go
@@ -8,10 +8,7 @@ import (
     "strconv"
 )
 
-
-func main() {
-    //var matrix [49][49] boolean
-
+func getLinks() {
     var fp *os.File
     var err error
 
@@ -24,18 +21,33 @@ func main() {
     scanner := bufio.NewScanner(fp)
     links := [][]int{}
     cnt := 0
+
     for scanner.Scan() {
+        // Read a line
         line := scanner.Text()
+
+        // Split A\tB to [A, B]
         array := strings.Split(line, "\t")
+
+        // Convert each number to int
         from, _ := strconv.Atoi(array[0])
         to, _ := strconv.Atoi(array[1])
+
         link := []int{from, to}
         links = append(links, link)
 
         cnt++
     }
+
     if err := scanner.Err(); err != nil {
         panic(err)
     }
+
     fmt.Println(links)
+}
+
+
+func main() {
+    //var matrix [49][49] boolean
+    getLinks()
 }
